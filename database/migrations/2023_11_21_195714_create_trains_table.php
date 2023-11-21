1e8f04a5e1a8b9e63126f2870f8c5504034a8554
Vahidone <v.ban.roma@gmail.com>
2023-11-21 21:13:29 +0100

    feat: add columns to table

--- database/migrations/2023_11_21_195714_create_trains_table.php
+++ database/migrations/2023_11_21_195714_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 50);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->date('departure_time');
+            $table->date('arrival_time');
+            $table->tinyInteger('train_code')->unsigned();
+            $table->tinyInteger('carriage_number')->unsigned();
+            $table->boolean('on_time');
+            $table->boolean('canceled');
             $table->timestamps();
         });
     }
